c20f0c579f181a077e4f79d2c9768c911d8b9ba9
Brian Sam Thomas <thebrainchild95@gmail.com>
2019-02-05 16:31:18 +0530

    updated github link in footer

--- 1. JTP (Online Learning)/admin/footer.php
+++ 1. JTP (Online Learning)/admin/footer.php
@@ -2,7 +2,7 @@
 <footer class="sticky-footer mybgcolor border-top border-warning">
   <div class="container my-auto">
     <div class="copyright text-center my-auto">
-      <span>JTP &copy; Powered by <a style="color: #243E60" href="https://brainchild95.github.io/WEBDEVProjectsByBKiD">BkiD</a></span>
+      <span>JTP &copy; Powered by <a style="color: #243E60" href="https://github.com/BRAiNCHiLD95/">BkiD</a></span>
     </div>
   </div>
 </footer>
