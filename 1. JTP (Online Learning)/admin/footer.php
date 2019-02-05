e28b0f425cd66a97aa87e654465bea4b01f53776
Brian Sam Thomas <thebrainchild95@gmail.com>
2019-02-05 13:26:42 +0530

    Added footer code for all backend pages

---
+++ 1. JTP (Online Learning)/admin/footer.php
@@ -0,0 +1,8 @@
+<!-- Sticky Footer -->
+<footer class="sticky-footer mybgcolor border-top border-warning">
+  <div class="container my-auto">
+    <div class="copyright text-center my-auto">
+      <span>JTP &copy; Powered by <a style="color: #243E60" href="https://brainchild95.github.io/WEBDEVProjectsByBKiD">BkiD</a></span>
+    </div>
+  </div>
+</footer>
