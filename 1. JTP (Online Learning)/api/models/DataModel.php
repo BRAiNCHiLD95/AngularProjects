3903f300640b0781888a0b8638e8dbe868bb3d3a
Brian Sam Thomas <thebrainchild95@gmail.com>
2019-02-05 13:22:41 +0530

    Data Models for API responses

---
+++ 1. JTP (Online Learning)/api/models/DataModel.php
@@ -0,0 +1,121 @@
+<?php
+  class DataModel {
+    private $conn;
+    private $table = "contents";
+
+    // content properties
+
+    public $content_id;
+    public $subject_id;
+    public $subtopic_id;
+    public $subject;
+    public $subtopic;
+    public $title;
+    public $body;
+    public $createdAt;
+
+    public function __construct($db) {
+        $this->conn = $db;
+    }
+
+    // Fetch Content
+
+    public function getAllContent() {
+      $query = 'SELECT 
+                  s.subject_name as subject_name,
+                  sub.subtopic_name as subtopic_name,
+                  c.content_id as content_id,
+                  c.subject_id as subject_id,
+                  c.content_title as content_title,
+                  c.created_at as created_at
+                FROM
+                  '. $this->table .' c
+                INNER JOIN
+                  subjects s ON s.subject_id = c.subject_id
+                INNER JOIN
+                  subtopics sub on sub.subtopic_id = c.subtopic_id
+                ORDER BY
+                  c.created_at DESC
+                LIMIT 20';
+          
+      $statement = $this->conn->prepare($query);
+      $statement->execute();
+
+      return $statement;
+    }
+
+    // Fetch All Subjects
+    public function getAllSubjects() {
+      $query = 'SELECT * FROM `subjects`';
+      $statement = $this->conn->prepare($query);
+      $statement->execute();
+
+      return $statement;
+    }
+
+    // Fetch All Subtopics
+    public function getAllSubtopics() {
+      $query = 'SELECT 
+                s.subtopic_id as subtopic_id,
+                s.subtopic_name as subtopic_name,
+                c.content_id as content_id,
+                c.content_title as content_title 
+                FROM subtopics s
+                INNER JOIN '. $this->table .' c
+                ON c.subtopic_id = s.subtopic_id
+                WHERE s.subject_id = ?';
+      $statement = $this->conn->prepare($query);
+      $statement->bindParam(1, $this->subject_id);
+      
+      $statement->execute();
+      
+      return $statement;
+    }
+    
+    public function getSingleContent() {
+      $query = 'SELECT 
+                  s.subject_name as subject_name,
+                  sub.subtopic_name as subtopic_name,
+                  c.content_id as content_id,
+                  c.content_title as content_title,
+                  c.content_body as content_body,
+                  c.created_at as created_at
+                FROM
+                  '. $this->table .' c
+                INNER JOIN
+                  subjects s ON s.subject_id = c.subject_id
+                INNER JOIN
+                  subtopics sub on sub.subtopic_id = c.subtopic_id
+                WHERE
+                  c.content_id = ?
+                LIMIT 0,1';
+          
+      $statement = $this->conn->prepare($query);
+      $statement->bindParam(1, $this->id);
+      $statement->execute();
+
+      $row = $statement->fetch(PDO::FETCH_ASSOC);
+      $this->content_id = $row['content_id'];
+      $this->subject = $row['subject_name'];
+      $this->subtopic = $row['subtopic_name'];
+      $this->title = $row['content_title'];
+      $this->body = $row['content_body'];
+      $this->createdAt = $row['created_at'];
+    }
+
+  public function getAllTitles() {
+    $query = 'SELECT 
+                c.content_title as content_title,
+                c.content_id as content_id,
+                c.subtopic_id as subtopic_id
+              FROM '. $this->table .' c
+              WHERE c.subtopic_id = ?';
+    $statement = $this->conn->prepare($query);
+    $statement->bindParam(1, $this->subtopic_id);
+    
+    $statement->execute();
+    
+    return $statement;
+  }
+}
+?>
